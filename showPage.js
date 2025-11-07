050a3cdd0d090891511f880241e861ffb277c3cf
Miguel Angel Fuente Rosado <80107173+MiguelAngel456@users.noreply.github.com>
2025-11-07 18:14:24 +0100

    Por mejorar

---
+++ showPage.js
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   showPage.js                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfuente- <mfuente-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 15:50:28 by mfuente-          #+#    #+#             */
+/*   Updated: 2025/11/07 16:54:59 by mfuente-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+async function show(page) {
+    //const res = await fetch(page);
+    //const html = await res.text();
+    //app.innerHTML = html;
+    const f = document.getElementById('app');
+    f.src = page; // Establece el src del iframe
+    
+    f.onload = () => {
+        f.contentWindow.focus();
+    };
+    
+    if (page === './html/index.html') {
+        const script = document.createElement('script');
+        script.src = 'pong_refactored.js';
+        document.body.appendChild(script);
+    }
+}
+//show('./html/menuContent.html');
